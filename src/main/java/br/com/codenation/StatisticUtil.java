f0fa00af1f5c3e80726fceb656b6f3483d86708c
Luis Sukys <luis.sukys@sippulse.com>
2020-06-26 12:28:41 -0300

    desafio superado

--- src/main/java/br/com/codenation/StatisticUtil.java
+++ src/main/java/br/com/codenation/StatisticUtil.java
@@ -1,16 +1,101 @@
 package br.com.codenation;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StatisticUtil {
 
+	/**
+	 * Média:
+	 * 
+	 * A média pode ser obtida através da soma de todos os valores do conjunto,
+	 * dividida pela quantidade de elementos do conjunto.
+	 * 
+	 * Exemplo: Para um array [1,2,3,4,5], o cálculo da média deverá ser: 15 / 5 = 3
+	 * (soma / quantidade = média)
+	 * 
+	 * @param elements
+	 * @return
+	 */
 	public static int average(int[] elements) {
-		return 0;
+		int total = 0;
+		for (int integer : elements) {
+			total += integer;
+		}
+		return total / elements.length;
 	}
 
+	/**
+	 * Moda:
+	 * 
+	 * A moda é representada pelo valor que mais se repete no conjunto.
+	 * 
+	 * Exemplo: Para um array [1,2,2,3,3,4,4,4,5,5,6,7,8], a moda é o número 4 com 3
+	 * ocorrências.
+	 * 
+	 * Observação: Para fins de simplificação, não deverão ser considerados arrays
+	 * que possuam mais de 1 número de moda, como por exemplo: [1,1,2,2,4]
+	 * 
+	 * @param elements
+	 * @return
+	 */
 	public static int mode(int[] elements) {
-		return 0;
+		Map<Integer, Integer> qtd = new HashMap<>();
+		for (int i : elements) {
+			if (!qtd.containsKey(i)) {
+				qtd.put(i, 1);
+			} else {
+				qtd.replace(i, qtd.get(i) + 1);
+			}
+		}
+		Integer key = 0;
+		Integer max = null;
+		for (Integer item : qtd.keySet()) {
+			if (max == null) {
+				key = item;
+				max = qtd.get(item);
+			} else if (qtd.get(item) > max) {
+				key = item;
+				max = qtd.get(item);
+			}
+		}
+		return key;
 	}
 
+	/**
+	 * Mediana:
+	 * 
+	 * A mediana pode ser obtida ordenando os valores no array e pegando o valor que
+	 * se encontra no meio do mesmo. Para arrays com quantidade par de elementos,
+	 * não haverá um único valor no meio, nesse caso a mediana será definida como a
+	 * média entre os dois valores do meio do array.
+	 * 
+	 * Exemplo: Para um array [1,2,3,4,5,6,7], a mediana é representada pelo número
+	 * 4 que se encontra exatamente no meio da distribuição. Para um array
+	 * [1,4,6,10,12,14], a mediana é definida pelo cálculo da média entre 6 e 10, o
+	 * que significa que teremos: (6 + 10) / 2 = 8
+	 * 
+	 * @param elements
+	 * @return
+	 */
 	public static int median(int[] elements) {
-		return 0;
+		List<Integer> lista = Arrays.stream(elements).boxed().sorted().collect(Collectors.toList());
+		int mediana = 0;
+		if (lista.size() % 2 == 0) {
+			mediana += lista.get(Math.floorDiv(lista.size(), 2) - 1);
+			mediana += lista.get(Math.floorDiv(lista.size(), 2));
+			if(mediana > 0) {
+				mediana = Math.floorDiv(mediana, 2);
+			} else {
+				mediana = Math.floorDiv(mediana *-1, 2) * -1;
+			}
+		} else {
+			int index = Math.floorDiv(lista.size(), 2);
+			mediana = lista.get(index);
+		}
+		return mediana;
 	}
 }
--- src/test/java/br/com/codenation/StatisticUtilTest.java
+++ src/test/java/br/com/codenation/StatisticUtilTest.java
@@ -8,16 +8,56 @@ public class StatisticUtilTest {
 
 	@Test
 	public void testAverage() {
-		assertEquals(3, StatisticUtil.average(new int[] {1,2,3,4,5}));
+		assertEquals(3, StatisticUtil.average(new int[] { 1, 2, 3, 4, 5 }));
+	}
+
+	@Test
+	public void testAverage2() {
+		assertEquals(3, StatisticUtil.average(new int[] { 5, 4, 2, 1, 3 }));
+	}
+
+	@Test
+	public void testAverage3() {
+		assertEquals(4, StatisticUtil.average(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 1 }));
 	}
 
 	@Test
 	public void testMode() {
-		assertEquals(3, StatisticUtil.mode(new int[] {1,2,3,3}));
+		assertEquals(3, StatisticUtil.mode(new int[] { 1, 2, 3, 3 }));
 	}
-	
+
+	@Test
+	public void testMode2() {
+		assertEquals(3, StatisticUtil.mode(new int[] { 3, 2, 1, 3 }));
+	}
+
+	@Test
+	public void testMode3() {
+		assertEquals(4, StatisticUtil.mode(new int[] { 1, 2, 3, 3, 4, 4, 4, 5, 5, 5, 4 }));
+	}
+
 	@Test
 	public void testMedian() {
-		assertEquals(3, StatisticUtil.median(new int[] {1,2,3,4,5}));
+		assertEquals(3, StatisticUtil.median(new int[] { 1, 2, 3, 4, 5 }));
+	}
+
+	@Test
+	public void testMedian2() {
+		assertEquals(5, StatisticUtil.median(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
+	}
+
+	@Test
+	public void testMedian3() {
+		assertEquals(8, StatisticUtil.median(new int[] { 1, 4, 6, 10, 12, 14 }));
+	}
+
+	@Test
+	public void testMedian5() {
+		assertEquals(6, StatisticUtil.median(new int[] { -1, 6, 10, 4, 12, 14, 1 }));
+	}
+
+	@Test
+	public void testMedian6() {
+		assertEquals(-1, StatisticUtil.median(new int[] { -6, 10, -4, 12, -14, 1 }));
 	}
 }
